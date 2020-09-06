08a0b0c046724e404046a345fcc77b09a47b2e78
Henry <cryptix@riseup.net>
2020-09-06 19:37:23 +0200

    implement getTangle

--- sbot/new.go
+++ sbot/new.go
@@ -347,7 +347,7 @@ func initSbot(s *Sbot) (*Sbot, error) {
 					feeds.(*roaring.MultiLog),
 					byType.(*roaring.MultiLog),
 					tangles.(*roaring.MultiLog),
-					s.RootLog)
+					s.RootLog, s)
 				s.public.Register(plug)
 				s.master.Register(plug)
 			}
