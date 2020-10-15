988edfe9b9e9610d2510451efd6c31b6338082d2
Henry <cryptix@riseup.net>
2020-10-15 20:47:33 +0200

    update dataset
    has a new structure, state isnt used yet
    
    ps: i had to filter the (invalid) hmac key stuff from the data because
    it also includes broken base64 data, our api doesn't need to validate
    that. ( jq '.[] | select(.hmacKey == null)' testdata-fuzzed.json )

--- message/legacy/verify.go
+++ message/legacy/verify.go
@@ -7,6 +7,7 @@ import (
 	"crypto/sha256"
 	"encoding/json"
 	"io"
+	"strings"
 
 	"github.com/pkg/errors"
 	refs "go.mindeco.de/ssb-refs"
@@ -51,6 +52,39 @@ func Verify(raw []byte, hmacSecret *[32]byte) (*refs.MessageRef, *DeserializedMe
 
 	if dmsg.Hash != "sha256" {
 		return nil, nil, errors.Errorf("ssb Verify: scuttlebutt happend anyway")
+	}
+
+	if n := len(dmsg.Content); n < 1 {
+		return nil, nil, errors.Errorf("ssb Verify: has no content (%d)", n)
+	}
+
+	switch dmsg.Content[0] {
+	case '{':
+		var typedContent struct {
+			Type string
+		}
+		err = json.Unmarshal(dmsg.Content, &typedContent)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		if tlen := len(typedContent.Type); tlen < 3 || tlen > 53 {
+			return nil, nil, errors.Errorf("ssb Verify: scuttlebutt v1 requires a type field: %q", typedContent.Type)
+		}
+
+	case '"':
+		var justString string
+		err = json.Unmarshal(dmsg.Content, &justString)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		if !strings.HasSuffix(justString, ".box") && !strings.HasSuffix(justString, ".box2") {
+			return nil, nil, errors.Errorf("ssb Verify: scuttlebutt v1 private messages need to have the right suffix")
+		}
+
+	default:
+		return nil, nil, errors.Errorf("ssb Verify: unexpected content: %q", dmsg.Content[0])
 	}
 
 	woSig, sig, err := ExtractSignature(enc)
